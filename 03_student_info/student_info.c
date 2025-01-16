1ca71bfef634ac4c857973034d631694d4e5f4da
d9nniw <dward9407@floridapoly.edu>
2025-01-16 16:52:29 +0000

    variables

---
+++ 03_student_info/student_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void) {
+   
+    int course_id = 2271;
+    int student_id = 10324;
+    printf("Name: Daniel Ward\n");
+    printf("Course ID: %d\n", course_id );
+    printf("Student Id %d\n", student_id);
+    printf ("Course ID: %d\nStudent ID: %d\n", course_id, student_id);
+    printf ("Student ID - Course ID is: %d\n",student_id - course_id);
+    return 0;
+}
