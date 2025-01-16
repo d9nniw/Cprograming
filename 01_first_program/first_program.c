28b87fc3e45aa646c29dcf278f37518e50188dbc
d9nniw <dward9407@floridapoly.edu>
2025-01-16 16:27:44 +0000

    first program uploaded

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,19 @@
+// one-line comment 
+// This is the first program which print a welcoming message.
+
+/*
+
+Multiline comment.
+
+January 9 2025
+
+*/
+
+#include <stdio.h>
+
+// Create a main function.
+int main() {
+    printf(" Welcome\n to\n COP2271!!\n");
+    printf(" I\n Want\n To\n Code\n");
+    return 0;
+}
